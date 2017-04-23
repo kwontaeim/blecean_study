9da27b8c0e3da4d5b0877a0e6b0921176212254e
Taeim Kwon <k.taemee@gmail.com>
2017-04-23 19:02:56 +0900

    .

--- blucean_study/src/ReadExcel.java
+++ blucean_study/src/ReadExcel.java
@@ -1,7 +1,57 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcel {
+
+  private static final String FILE_PATH = "/Users/KWON/Downloads/로또 당첨번호(1-750).xlsx";
+
+  public static void main(String[] args) throws Exception {
+
+    // 엑셀파일 불러오기 
+    File file = new File(FILE_PATH);
+
+    int[] numData = new int[6]; 
+    int[] index = new int[46];
+    HashMap<String, String> map  = new HashMap<String, String>();
+
+
+
+    // 엑셀파일 오픈 
+    XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(file));
+
+    for (Row row : wb.getSheetAt(0)) {
+
+      // 처음 3열은 생략 
+      if (row.getRowNum() <= 2) {
+        continue;
+      }
+      // 회자별 로또번호 저장 
+      for (int i = 13; i <= 18; i++) {
+        numData[i-13] = (int)row.getCell(i).getNumericCellValue();
+        //System.out.println(numData[i-13]);
+      }
+
+      for (int i = 0; i < numData.length; i++) {
+        index[numData[i]]++; //COUNT          
+      }
+    }
+    
+    //숫자별 카운트를 맵 객체에 담기
+    
+    for (int i = 0; i < index.length; i++) {
+      map.put        
+    }
   
- public static void main(String[] args) {
-   
- }
+
+    
+    
+    wb.close();
+  }
+  
+ 
+
 }
