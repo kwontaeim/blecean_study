e891e7683da9e35be3a873abc93f83dc6f84fde6
Taeim Kwon <k.taemee@gmail.com>
2017-05-07 20:44:55 +0900

    .

---
+++ src/IntegerCouple.java
@@ -0,0 +1,45 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class IntegerCouple {
+  
+ static int doCompare (HashSet<Integer> num, int gap) {
+   int count = 0;
+   
+   Iterator<Integer> it = num.iterator();
+   
+   while (it.hasNext()) {
+     if (num.contains(it.next() + gap))
+       count++;
+   }
+   return count;
+ }
+  
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int max = sc.nextInt();
+    int gap = sc.nextInt();
+    sc.nextLine(); // 엔터처리 
+    String[] inputNum  = sc.nextLine().split(" ");
+    
+    HashSet<Integer> num = new HashSet<Integer>();
+    
+    for (int i = 0; i < inputNum.length; i++) {
+      num.add(Integer.parseInt(inputNum[i]));
+    }
+    
+    sc.close();
+    
+  System.out.println(max);
+  System.out.println(gap);
+  
+  System.out.println(doCompare(num, gap));
+    
+ 
+    
+  }
+
+}
